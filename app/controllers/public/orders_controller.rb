8eb732a7ecfde060ca367f90facedcfee8a5fa07
EC2 Default User <ec2-user@ip-172-31-25-157.ap-northeast-1.compute.internal>
2022-12-20 13:36:25 +0000

    address情報編集

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,27 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new(order_params)
+    @addresses = current_member.addresses.all
   end
 
   def comfirm
+    @order = Order.new(order_params)
+    if params[:order][:address_option] == "0"
+      @order.shipping_post_code = current_customer.post_code
+      @order.shipping_address = current_customer.address
+      @order.shipping_name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_option] == "1"
+      ship = Address.find(params[:order][:customer_id])
+      @order.shipping_post_code = ship.post_code
+      @order.shipping_address = ship.address
+      @order.shipping_name = ship.name
+    elsif params[:order][:address_option] = "2"
+      @order.shipping_post_code = params[:order][:shipping_post_code]
+      @order.shipping_address = params[:order][:shipping_address]
+      @order.shipping_name = params[:order][:shipping_name]
+    else
+      render 'new'
+    end
   end
 
   def complite
@@ -12,5 +31,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:postage, :payment_method, :shipping_name, :shipping_address, :shipping_post_code, :customer_id, :total_payment, :status)
   end
 end
