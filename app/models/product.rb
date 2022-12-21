90eb4ef7de8d8a2bfa69b34356ffb97577de1c07
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-12-21 03:49:19 +0000

    public_products_show追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,5 @@
 class Product < ApplicationRecord
-
   has_many :cart_items
-
+  enum is_active: { on_sale: 0, out_of_sale: 1 }
   has_one_attached :image
-
-  def get_image
-    unless image.attached?
-      file_path Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image
-  end
-
 end
