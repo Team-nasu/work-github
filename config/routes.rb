3c5212b9d01c4a2946504a3f2ab3aef40cabdb84
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-12-21 09:07:12 +0000

    public_products_show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+#   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+#   sessions: "admin/sessions"
+# }
+#   devise_for :customers,skip: [:passwords], controllers: {
+#   registrations: "public/registrations",
+#   sessions: 'public/sessions'
+# }
 
   namespace :admin do
     resources :customers, only: [:index, :show, :edit]
