348ba0706e3608f1eb9c4598c86bfa73360952ac
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-12-22 13:10:33 +0000

    kato

--- db/migrate/20221218025042_create_cart_items.rb
+++ db/migrate/20221218025042_create_cart_items.rb
@@ -2,7 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
       t.integer :product_id
-      t.integer :user_id
+      t.integer :customer_id
       t.integer :quantity
       t.timestamps
     end
