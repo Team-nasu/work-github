7d8ed4c12ea08a11ede46404ef2a947918ee55e3
ktz1998 <doraemonfan.kosuke@gmail.com>
2022-12-23 07:38:42 +0000

    カートアイテムアソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   end
 
   scope module: :public do
-    resources :addresses, only: [:index, :edit]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
     post 'orders/confirm' => "orders#confirm"
     get "customers/information/edit" => "customers#edit"
     get "customers" => "customers#show"
