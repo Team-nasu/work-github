fb407c106ee962d50efb99659cd4c2c74c29cb6a
ktz1998 <doraemonfan.kosuke@gmail.com>
2022-12-24 12:25:28 +0000

    cart_itemモデル修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,9 @@ class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :customer
 
+  def with_tax_price
+    (price * 1.1).floor
+  end
 
   def subtotal
     product.with_tax_price * quantity
