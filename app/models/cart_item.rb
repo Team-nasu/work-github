51d4382d81f8969b3ab8dc977f8db8b0e9ddcd5f
EC2 Default User <ec2-user@ip-172-31-25-157.ap-northeast-1.compute.internal>
2022-12-25 02:18:14 +0000

    修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,10 +2,13 @@ class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :customer
 
-
-  def subtotal
-    product.with_tax_price * quantity
-  end
+ def with_tax_price
+  (price * 1.1).floor
+ end
+ 
+ def subtotal
+   product.with_tax_price * quantity
+ end
 
 
 end
