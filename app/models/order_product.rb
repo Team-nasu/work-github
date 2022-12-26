a91fc505b1ae816ed5c471dec849f259f7534d51
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-12-26 10:22:06 +0000

    public_products_show

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,6 +1,11 @@
 class OrderProduct < ApplicationRecord
     belongs_to :order
     belongs_to :product
+
+    enum making_status: {
+     unable_to_begin: 0, waiting_for_production: 1, in_production: 2, completion_of_production: 3
+    }
+
     def subtotal
         price * amount
     end
